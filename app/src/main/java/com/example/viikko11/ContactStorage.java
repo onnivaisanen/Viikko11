e7ce43656cfaf86f5d9f606323f2721988d2b05c
onnivaisanen <onni.vaisanen@student.lut.fi>
2025-03-23 20:31:33 +0200

    Viikko11

--- app/src/main/java/com/example/viikko11/ContactStorage.java
+++ app/src/main/java/com/example/viikko11/ContactStorage.java
@@ -3,6 +3,7 @@ package com.example.viikko11;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class ContactStorage {
     private ArrayList<Contact> contacts = new ArrayList<>();
@@ -11,6 +12,7 @@ public class ContactStorage {
     private ContactStorage() {
         contacts = new ArrayList<>();
     }
+
     public static ContactStorage getInstance() {
         if (instance == null) {
             instance = new ContactStorage();
@@ -33,11 +35,23 @@ public class ContactStorage {
 
     }
 
+    public void removeContactsStartingWithLetter(String letter) {
+        Iterator<Contact> iterator = ContactStorage.getInstance().getContacts().iterator();
+
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getFirstName().startsWith(letter)) {
+                iterator.remove();
+            }
+        }
+    }
+
     public void sortAlphabetically() {
         Collections.sort(contacts, (c1, c2) -> c1.getFullName().compareToIgnoreCase(c2.getFullName()));
     }
 
     public void sortByGroup() {
-        contacts.sort(Comparator.comparing(Contact::getContactGroup));
+        contacts.sort(Comparator.comparing(Contact::getContactGroup, String::compareToIgnoreCase));
     }
+
 }
